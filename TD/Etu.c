11231d1de0c7a5c326e8f2cff7799aace684ee23
zhenyuefu <zhenyue.fu@etu.upmc.fr>
2020-10-28 14:53:37 +0000

    td4

---
+++ TD/Etu.c
@@ -0,0 +1,21 @@
+#include "Etu.h"
+ty_etu *lecture_ascii_etu(char *nom_fichier, int *nb_etu) {
+  FILE *pf;
+  ty_etu *tab_etu;
+
+  pf = fopen(nom_fichier, "r");
+  if (pf == NULL) {
+    fprintf(stderr, "Impossible ouvrir %s\n", nom_fichier);
+    return NULL;
+  }
+  if (fscanf(pf, " %d", &nb_etu) != 1) {
+    fprintf(stderr, "Error");
+    fclose(pf);
+    return NULL;
+  }
+  tab_etu malloc(nb_etu * sizeof(ty_etu));
+  for (i = 0; i < *nb_etu; i++) {
+    fscanf(pf, " %d %s %s %d", &(tab_etu[i].id_etu), &(tab_etu[i].nom),
+           &(tab_etu[i].prenom), &(tab_etu[i].nb_ue));
+  }
+}
---
+++ TD/Etu.h
@@ -0,0 +1,8 @@
+typedef struct _ty_etu {
+  int id_etu;
+  char nom[200];
+  char prenom[200];
+  int nb_ue;
+  char codes_ue[9][20];
+  int notes[20];
+} ty_etu;

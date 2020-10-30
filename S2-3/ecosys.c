72edc85a424f6f68304e590c3eaa970dfdc85c2b
Zhenyue Fu <zhenyuef@gmail.com>
2020-10-30 23:28:19 +0100

    完成tme2

--- S2-3/ecosys.c
+++ S2-3/ecosys.c
@@ -1,4 +1,5 @@
 #include "ecosys.h"
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,27 +28,37 @@ Animal *ajouter_en_tete_animal(Animal *liste, Animal *animal) {
 
 /* A faire. Part 1, exercice 5, question 1 */
 void ajouter_animal(int x, int y, float energie, Animal **liste_animal) {
-  /*A Completer*/
+  assert(x < SIZE_X && x > 0);
+  assert(y < SIZE_Y && y > 0);
+  Animal *new = creer_animal(x, y, energie);
+  new->suivant = *liste_animal;
+  *liste_animal = new;
 }
 
 /* A Faire. Part 1, exercice 5, question 5 */
 void enlever_animal(Animal **liste, Animal *animal) {
-  /*A Completer*/
-
-  return;
+  Animal *current;
+  while ((current = *liste) != NULL && current->suivant != animal) {
+    liste = &current->suivant;
+  }
+  *liste = current->suivant;
+  free(current);
 }
 
 /* A Faire. Part 1, exercice 5, question 2 */
 Animal *liberer_liste_animaux(Animal *liste) {
-  /*A Completer*/
-
+  Animal *current;
+  Animal *suivantp = liste;
+  while ((current = suivantp) != NULL) {
+    suivantp = current->suivant;
+    free(current);
+  }
   return NULL;
 }
 
 /* Fourni: part 1, exercice 3, question 4 */
 unsigned int compte_animal_rec(Animal *la) {
-  if (!la)
-    return 0;
+  if (!la) return 0;
   return 1 + compte_animal_rec(la->suivant);
 }
 
@@ -69,49 +80,87 @@ void afficher_ecosys(Animal *liste_proie, Animal *liste_predateur) {
   Animal *pa = NULL;
 
   /* on initialise le tableau */
-  /*A Completer*/
+  for (i = 0; i < SIZE_X; ++i) {
+    for (j = 0; j < SIZE_Y; ++j) {
+      ecosys[i][j] = ' ';
+    }
+  }
 
   /* on ajoute les proies */
-  /*A Completer*/
+  pa = liste_proie;
+  while (pa) {
+    ++nbproie;
+    assert(pa->x >= 0 && pa->x < SIZE_X && pa->y >= 0 && pa->y < SIZE_Y);
+    ecosys[pa->x][pa->y] = '*';
+    pa = pa->suivant;
+  }
 
   /* on ajoute les predateurs */
-  /*A Completer*/
+  pa = liste_predateur;
+  while (pa) {
+    ++nbpred;
+    assert(pa->x >= 0 && pa->x < SIZE_X && pa->y >= 0 && pa->y < SIZE_Y);
+    if ((ecosys[pa->x][pa->y] == '@') || (ecosys[pa->x][pa->y] == '*')) { /*
+    proies aussi present */
+      ecosys[pa->x][pa->y] = '@';
+    } else {
+      ecosys[pa->x][pa->y] = 'O';
+    }
+    pa = pa->suivant;
+  }
 
   /* on affiche le tableau */
-  /*A Completer*/
+  printf("+");
+  for (j = 0; j < SIZE_Y; ++j) {
+    printf("-");
+  }
+  printf("+\n");
+  for (i = 0; i < SIZE_X; ++i) {
+    printf("|");
+    for (j = 0; j < SIZE_Y; ++j) {
+      putchar(ecosys[i][j]);
+    }
+    printf("|\n");
+  }
+  printf("+");
+  for (j = 0; j < SIZE_Y; ++j) {
+    printf("-");
+  }
+  printf("+\n");
+  printf("Nb proies : %5d\tNb predateurs : %5d\n", nbproie, nbpred);
 }
 
 void clear_screen() {
   printf("\x1b[2J\x1b[1;1H"); /* code ANSI X3.4 pour effacer l'ecran */
 }
 
-/* PARTIE 2*/
+// /* PARTIE 2*/
 
-/* Part 2. Exercice 5, question 1 */
-void bouger_animaux(Animal *la) { /*A Completer*/
-}
+// /* Part 2. Exercice 5, question 1 */
+// void bouger_animaux(Animal *la) { /*A Completer*/
+// }
 
-/* Part 2. Exercice 5, question 3 */
-void reproduce(Animal **liste_animal, float p_reproduce) { /*A Completer*/
-}
+// /* Part 2. Exercice 5, question 3 */
+// void reproduce(Animal **liste_animal, float p_reproduce) { /*A Completer*/
+// }
 
-/* Part 2. Exercice 7, question 1 */
-void rafraichir_proies(Animal **liste_proie, int monde[SIZE_X][SIZE_Y]) {
-  /*A Completer*/
-}
+// /* Part 2. Exercice 7, question 1 */
+// void rafraichir_proies(Animal **liste_proie, int monde[SIZE_X][SIZE_Y]) {
+//   /*A Completer*/
+// }
 
-/* Part 2. Exercice 8, question 1 */
-Animal *animal_en_XY(Animal *l, int x, int y) {
-  /*A Completer*/
+// /* Part 2. Exercice 8, question 1 */
+// Animal *animal_en_XY(Animal *l, int x, int y) {
+//   /*A Completer*/
 
-  return NULL;
-}
+//   return NULL;
+// }
 
-/* Part 2. Exercice 8, question 2 */
-void rafraichir_predateurs(Animal **liste_predateur, Animal **liste_proie) {
-  /*A Completer*/
-}
+// /* Part 2. Exercice 8, question 2 */
+// void rafraichir_predateurs(Animal **liste_predateur, Animal **liste_proie) {
+//   /*A Completer*/
+// }
 
-/* Part 2. Exercice 6, question 2 */
-void rafraichir_monde(int monde[SIZE_X][SIZE_Y]) { /*A Completer*/
-}
+// /* Part 2. Exercice 6, question 2 */
+// void rafraichir_monde(int monde[SIZE_X][SIZE_Y]) { /*A Completer*/
+// }
--- S2-3/main_tests.c
+++ S2-3/main_tests.c
@@ -1,21 +1,54 @@
 #include <assert.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "ecosys.h"
+#include <unistd.h>
 
+#include "ecosys.h"
 
 #define NB_PROIES 20
 #define NB_PREDATEURS 20
 
-
-
-
-
 int main(void) {
+  int i;
+  int x, y;
+  float energie;
+  time_t t;
+  Animal *liste_proie = NULL;
+  Animal *liste_predateur = NULL;
+  Animal *delete_proie = NULL;
+  Animal *delete_predateur = NULL;
 
-  /* A Completer. Exercice 5, question 3 */
+  srand((unsigned)time(&t));
 
+  for (i = 0; i < NB_PROIES; i++) {
+    x = rand() % (SIZE_X - 1) + 1;
+    y = rand() % (SIZE_Y - 1) + 1;
+    energie = rand() % 10;
+    ajouter_animal(x, y, energie, &liste_proie);
+  }
+
+  for (i = 0; i < NB_PREDATEURS; i++) {
+    x = rand() % (SIZE_X - 1) + 1;
+    y = rand() % (SIZE_Y - 1) + 1;
+    energie = rand() % 10;
+    ajouter_animal(x, y, energie, &liste_predateur);
+  }
+
+  afficher_ecosys(liste_predateur, liste_proie);
+  /*
+  ** Test function enlever_animal()
+  */
+  delete_predateur = liste_predateur;
+  delete_proie = liste_proie;
+  for (i = 0; i < 5; i++) {
+    delete_proie = delete_proie->suivant;
+    delete_predateur = delete_predateur->suivant;
+  }
+  enlever_animal(&liste_predateur, delete_predateur);
+  enlever_animal(&liste_proie, delete_proie);
+  afficher_ecosys(liste_predateur, liste_proie);
+  liberer_liste_animaux(liste_predateur);
+  liberer_liste_animaux(liste_proie);
   return 0;
 }

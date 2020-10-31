511e348a58a20176d6dd4c43c9faa2c21323daef
Zhenyue Fu <zhenyuef@gmail.com>
2020-10-31 13:09:24 +0100

    td3

--- TD/Etu.c
+++ TD/Etu.c
@@ -1,7 +1,11 @@
 #include "Etu.h"
+
+#include <stdio.h>
+
 ty_etu *lecture_ascii_etu(char *nom_fichier, int *nb_etu) {
   FILE *pf;
   ty_etu *tab_etu;
+  int i;
 
   pf = fopen(nom_fichier, "r");
   if (pf == NULL) {
@@ -13,7 +17,7 @@ ty_etu *lecture_ascii_etu(char *nom_fichier, int *nb_etu) {
     fclose(pf);
     return NULL;
   }
-  tab_etu malloc(nb_etu * sizeof(ty_etu));
+  tab_etu = malloc(*nb_etu * sizeof(ty_etu));
   for (i = 0; i < *nb_etu; i++) {
     fscanf(pf, " %d %s %s %d", &(tab_etu[i].id_etu), &(tab_etu[i].nom),
            &(tab_etu[i].prenom), &(tab_etu[i].nb_ue));
--- TD/Etu.h
+++ TD/Etu.h
@@ -1,3 +1,6 @@
+#define ETU_
+#ifdef ETU_
+
 typedef struct _ty_etu {
   int id_etu;
   char nom[200];
@@ -6,3 +9,7 @@ typedef struct _ty_etu {
   char codes_ue[9][20];
   int notes[20];
 } ty_etu;
+
+ty_etu *lecture_ascii_etu(char *nom_fichier, int *nb_etu);
+
+#endif
